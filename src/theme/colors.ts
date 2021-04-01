35980f6291ff919d1909fde4553ca950a308e083
sirawich.v <sirawich.v@siriustech.io>
2021-04-01 13:47:21 +0700

    feat(Styled): :sparkles: base theme set

---
+++ src/theme/colors.ts
@@ -0,0 +1,18 @@
+const colors: Record<string, string> = {
+  transparent: "transparent",
+  black: "#292929",
+  white: "#ffffff",
+  textColor: "#292929",
+  lightTextColor: "#757575",
+  inactiveField: "#F7F7F7",
+  inactiveColor: "#767676",
+  inactiveBG: "#e6e6e6",
+  borderColor: "#DBDBDB",
+  lightBorderColor: "#ededed",
+  primary: "#ffe268", 
+  primaryHover: "#ffb037",
+  turquoise: '#2f5d62',
+  turquoiseStrong: '#364547'
+};
+
+export default colors;
