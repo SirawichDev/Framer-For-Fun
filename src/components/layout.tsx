da7de3e06f9d78932d9a60a5d892950e94f9599d
sirawich.v <sirawich.v@siriustech.io>
2021-04-03 01:56:44 +0700

    feat(Styled): :sparkles: Put Layout Provider

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode, FC } from "react";
 import { ThemeProvider } from "styled-components";
+import Footer from "./footer/footer";
+import Navbar from "./navbar";
+import LayoutStyle from "./layout.style";
 import ResetCss from "./refresh-css";
 import { theme } from "../theme";
 
@@ -10,13 +13,21 @@ type LayoutProps = {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
-      <>
+      <LayoutStyle
+        initial={{ opacity: 0, x: "100px" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
         <ResetCss />
-        {children}
-      </>
+        <Navbar />
+        <div className="children">{children}</div>
+        <Footer>
+          Copyright &copy; {new Date().getFullYear()}
+          <a href="https://github.com/SirawichDev"> SirawichDev </a>
+        </Footer>
+      </LayoutStyle>
     </ThemeProvider>
   );
 };
-
 
 export default Layout;
