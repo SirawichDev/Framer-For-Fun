2f28c149510bcb06eeac53571e491a7c9c9ccb82
sirawich.v <sirawich.v@siriustech.io>
2021-04-03 01:55:01 +0700

    feat(Home): :recycle: set Garfield image to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Layout from './components/layout';
+import G3 from "./assets/images/ga3.jpg";
+import G4 from "./assets/images/ga4.jpg";
+
+import Layout from "./components/layout";
+import ImageWrapper from "./components/image/image.style";
 
 function App() {
   return (
     <Layout>
-      <h1>Hii</h1>
+      <ImageWrapper
+        initial={{ opacity: 0, x: '-100px' }}
+        animate={{ opacity: [1, 0, 1], x: 0 }}
+        transition={{ times: [0, 0.2, 1], duration: 5 }}
+      >
+        <img src={G3} alt="G3" className="rabbit" />
+        <img src={G4} alt="G4" className="rabbit" />
+      </ImageWrapper>
     </Layout>
   );
 }
